976927c3d273d00e2831a0b3b89f323858ebf4f1
Joshua Stresing <pyrotorch69@yahoo.com>
2022-01-27 16:35:29 -0800

    building out my logic

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import functions and grab DOM elements
+const raceSelect = document.getElementById('race-select');
+const worldSelect = document.getElementById('world-select');
+const terrainSelect = document.getElementById('terrain-select');
+
+const raceImage = document.getElementById('race-img');
+const worldImage = document.getElementById('world-img');
+const terrainImage = document.getElementById('terrain-img');
 
 // let state
+let slogans = [];
+let raceCounter = 0;
+let worldCounter = 0;
+let terrainCounter = 0; 
 
 // set event listeners 
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
